2f5c6b3f70b2ba4a106e4291f4a28facf11d3b7b
himynameisdave <d@velunny.com>
2017-06-13 20:11:35 -0700

    If it's Monday it will grab all the weekend entries and everything from Friday, closes #1

--- src/utils/get-dates.js
+++ src/utils/get-dates.js
@@ -1,8 +1,16 @@
 const moment = require('moment');
 
-const getDates = () => ({
-    START_DATE: moment().subtract(1, 'days').startOf('day').toISOString(),
-    END_DATE: moment().startOf('day').toISOString(),
-});
+const getDates = () => {
+    const MONDAY = 1;
+    let START_DATE = moment().subtract(1, 'days').startOf('day').toISOString();
+    //   If it's a Monday, grab all the entries that I've done since Friday morning
+    if (moment().day() === MONDAY) {
+        START_DATE = moment().subtract(3, 'days').startOf('day').toISOString();
+    }
+    return {
+        START_DATE,
+        END_DATE: moment().startOf('day').toISOString(),
+    };
+};
 
 module.exports = getDates;
